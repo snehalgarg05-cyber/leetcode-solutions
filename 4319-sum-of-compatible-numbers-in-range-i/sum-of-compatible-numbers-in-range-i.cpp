d76454138edaecd76eb6a35713cf43be2565e333
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-06-07 13:37:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (75.02%) - LeetSync

---
+++ 4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int sumOfGoodIntegers(int n, int k) {
+        int ans=0;
+        for(int i=1;i<=n+k;i++){
+            if(abs(n-i)<=k && (n&i)==0){
+                ans+=i;
+            }
+        }
+        return ans;
+    }
+};
