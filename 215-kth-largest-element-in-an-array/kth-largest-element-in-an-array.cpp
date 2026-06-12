58ed6024c529a9dd3b4f9549e2350fdd9c67d3b8
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-06-13 03:07:48 +0530

    Time: 23 ms (81.47%) | Memory: 72.7 MB (6.06%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -6,17 +6,34 @@
 //     }
 // };
 
-class Solution {//method-02:-priority queue
+// class Solution {//method-02:-priority queue
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int> pq(nums.begin(),nums.end());
+//         while(--k){
+//             pq.pop();
+//         }
+//         return pq.top();
+//     }
+// };
+
+
+class Solution {//method-03:-optimal
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq(nums.begin(),nums.end());
-        while(--k){
-            pq.pop();
+        priority_queue<int,vector<int>,greater<int>> p;
+        for(int i=0;i<k;i++){
+            p.push(nums[i]);
         }
-        return pq.top();
+        for(int i=k;i<nums.size();i++){
+            if(p.top()<nums[i]){
+                p.pop();
+                p.push(nums[i]);
+            }
+        }
+        return p.top();
     }
 };
-
 
 
 
