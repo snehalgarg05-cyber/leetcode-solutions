1022aceaa3e7cf6bf531a0728439871d94e003b2
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-06-13 02:11:27 +0530

    Time: 39 ms (33.29%) | Memory: 72 MB (6.87%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,7 +1,43 @@
-class Solution {
+// class Solution {  //method-01:-sorting
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         sort(nums.begin(),nums.end(),greater<int>());
+//         return nums[k-1];
+//     }
+// };
+
+class Solution {//method-02:-priority queue
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end(),greater<int>());
-        return nums[k-1];
+        priority_queue<int> pq(nums.begin(),nums.end());
+        while(--k){
+            pq.pop();
+        }
+        return pq.top();
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
