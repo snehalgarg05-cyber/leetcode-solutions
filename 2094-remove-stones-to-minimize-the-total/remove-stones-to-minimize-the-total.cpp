ee3cfa661d976cfc7f49e46d8241dad324ca0837
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-06-13 17:02:19 +0530

    Time: 206 ms (71.19%) | Memory: 102.9 MB (84.22%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
@@ -1,18 +1,50 @@
-class Solution {
+// class Solution {
+// public:
+//     int minStoneSum(vector<int>& piles, int k) {
+//        priority_queue<int> pq(piles.begin(),piles.end());
+//        while(k--){
+//         int store=pq.top();
+//         store-=floor(pq.top()/2);
+//         pq.pop();
+//         pq.push(store);
+//        } 
+//        int sum=0;
+//        while(!pq.empty()){
+//         sum+=pq.top();
+//         pq.pop();
+//        }
+//        return sum;
+//     }
+// };
+
+
+class Solution {//optimal
 public:
     int minStoneSum(vector<int>& piles, int k) {
-       priority_queue<int> pq(piles.begin(),piles.end());
-       while(k--){
-        int store=pq.top();
-        store-=floor(pq.top()/2);
-        pq.pop();
-        pq.push(store);
-       } 
-       int sum=0;
-       while(!pq.empty()){
-        sum+=pq.top();
-        pq.pop();
-       }
-       return sum;
+
+        // Max heap
+        priority_queue<int> pq(piles.begin(), piles.end());
+
+        long long sum = 0;
+
+        // Calculate initial sum
+        for (int x : piles) {
+            sum += x;
+        }
+
+        // Perform k operations
+        while (k--) {
+
+            int x = pq.top();
+            pq.pop();
+
+            int removed = x / 2;   // floor(x/2)
+
+            sum -= removed;
+
+            pq.push(x - removed);
+        }
+
+        return sum;
     }
 };
