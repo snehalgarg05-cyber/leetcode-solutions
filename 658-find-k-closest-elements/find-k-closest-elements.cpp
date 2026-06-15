5ae910327468a285b089f954a242b0eeece7b0c4
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-06-15 08:20:36 +0530

    Time: 17 ms (34.75%) | Memory: 38.7 MB (20.07%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -1,17 +1,42 @@
+// class Solution {
+// public:
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         vector<int> ans;
+//         int n = arr.size();
+//         priority_queue<
+//     pair<int,int>,
+//     vector<pair<int,int>>,
+//     greater<pair<int,int>>
+// > pq;
+//         for(int i=0;i<n;i++){
+//             pq.push({abs(arr[i]-x),arr[i]});
+//         }
+//         while(k--){
+//             ans.push_back(pq.top().second);
+//             pq.pop();
+//         }
+//         sort(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         vector<int> ans;
         int n = arr.size();
-        priority_queue<
-    pair<int,int>,
-    vector<pair<int,int>>,
-    greater<pair<int,int>>
-> pq;
-        for(int i=0;i<n;i++){
+        priority_queue<pair<int,int>> pq;
+        for(int i=0;i<k;i++){
             pq.push({abs(arr[i]-x),arr[i]});
         }
-        while(k--){
+        for(int i=k;i<n;i++){
+            if(pq.top().first>abs(arr[i]-x)){
+                pq.pop();
+                pq.push({abs(arr[i]-x),arr[i]});
+            }
+        }
+        while(!pq.empty()){
             ans.push_back(pq.top().second);
             pq.pop();
         }
@@ -19,3 +44,10 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+
+
