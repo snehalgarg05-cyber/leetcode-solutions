f4c253a9e6761dd85d8f82c9123c20024271173a
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-06-19 22:21:08 +0530

    Time: 3 ms (24.50%) | Memory: 12.7 MB (82.41%) - LeetSync

--- 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
+++ 1047-maximize-sum-of-array-after-k-negations/maximize-sum-of-array-after-k-negations.cpp
@@ -1,18 +1,48 @@
+// class Solution {
+// public:
+//     int largestSumAfterKNegations(vector<int>& nums, int k) {
+//         priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.end());
+//         int n = nums.size();
+//         while(k--){
+//             int store=pq.top();
+//             pq.pop();
+//             pq.push(-store);
+//         }
+//         int sum=0;
+//         while(!pq.empty()){
+//             sum+=pq.top();
+//             pq.pop();
+//         }
+//         return sum;
+//     }
+// };
+
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
+
         int n = nums.size();
-        while(k--){
-            int store=pq.top();
-            pq.pop();
-            pq.push(-store);
+
+        for(int i = 0; i < n && k > 0; i++) {
+            if(nums[i] < 0) {
+                nums[i] = -nums[i];
+                k--;
+            }
         }
-        int sum=0;
-        while(!pq.empty()){
-            sum+=pq.top();
-            pq.pop();
+
+        int sum = 0;
+        int mn = INT_MAX;
+
+        for(int x : nums) {
+            sum += x;
+            mn = min(mn, x);
         }
+
+        if(k % 2 == 1) {
+            sum -= 2 * mn;
+        }
+
         return sum;
     }
 };
