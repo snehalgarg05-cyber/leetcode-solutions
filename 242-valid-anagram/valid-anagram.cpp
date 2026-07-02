e6844307d45dbf122ce325549ea9bfb08284f887
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-07-02 15:12:13 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
-        }
-        else{
+        if(s.size() != t.size())
             return false;
+
+        unordered_map<char,int> mp;
+
+        for(char ch : s)
+            mp[ch]++;
+
+        for(char ch : t)
+            mp[ch]--;
+
+        for(auto m : mp){
+            if(m.second != 0)
+                return false;
         }
+
+        return true;
     }
 };
