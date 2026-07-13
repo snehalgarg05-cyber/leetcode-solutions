7f4b06a809879e2a322d1a9a79e6a71835540749
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-07-13 10:31:03 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (69.41%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,10 +1,20 @@
+// class Solution {//interview favourite appproach
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         k=k%n;
+//         reverse(nums.begin(),nums.end());
+//         reverse(nums.begin(),nums.begin()+k);
+//         reverse(nums.begin()+k,nums.end());
+//     }
+// };
+
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k=k%n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
+        std::rotate(nums.begin(),nums.end()-k,nums.end());
     }
 };
