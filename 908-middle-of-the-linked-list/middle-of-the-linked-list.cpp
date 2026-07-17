960bffe9847ef6ea0dfa1781fc4267b6e7ff60f8
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-07-17 21:37:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (64.54%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -8,22 +8,33 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         int size=0;
+//         ListNode*temp=head;
+//         while(temp!=NULL){
+//             size++;
+//             temp=temp->next;
+//         }
+//         int x=size/2;
+//         ListNode*curr=head;
+        
+//         while(x--){
+//             curr=curr->next;
+//         }
+//         return curr;
+//     }
+// };
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int size=0;
-        ListNode*temp=head;
-        while(temp!=NULL){
-            size++;
-            temp=temp->next;
+        ListNode*slow=head,*fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
         }
-        int x=size/2;
-        ListNode*curr=head;
-        
-        while(x--){
-            
-            curr=curr->next;
-        }
-        return curr;
+        return slow;
     }
 };
