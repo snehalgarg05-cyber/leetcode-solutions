0537d7fdc1f65fdd6fd67a561332c50a099f36f6
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-07-17 10:40:24 +0530

    Time: 39 ms (72.65%) | Memory: 45.4 MB (56.28%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,35 @@
-class Solution {
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             int sum=0;
+//             for(int j=i;j<n;j++){
+//                 sum+=nums[j];
+//                 if(sum==k){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+class Solution { //prefix sum
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
         int count=0;
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int cum_sum=0;
         for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k){
-                    count++;
-                }
+            cum_sum+=nums[i];
+            if(mp.find(cum_sum-k)!=mp.end()){
+                count+=mp[cum_sum-k];
             }
+            mp[cum_sum]++;
         }
         return count;
     }
