94ec4c8c66ca8bb476c45bcbdfe64605bb410906
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-08-01 02:30:02 +0530

    Time: 3 ms (88.86%) | Memory: 24 MB (30.96%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> result;
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<n;i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if(!result.empty() && end<=result.back()[1]){
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                if(intervals[j][0]<=end){
+                    end = max(end,intervals[j][1]);
+                }
+                else{
+                    break;
+                }
+            }
+            result.push_back({start,end});
+        }
+        
+        return result;
+    }
+};
