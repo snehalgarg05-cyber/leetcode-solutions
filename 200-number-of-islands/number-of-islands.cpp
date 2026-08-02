c473522d399c7f2409a4ad8dbac9c2e592536e29
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-08-03 01:04:14 +0530

    Time: 20 ms (94.07%) | Memory: 16.3 MB (70.59%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -7,9 +7,7 @@ public:
         if(i<0 || i>=m || j<0 ||j>=n || grid[i][j]!='1'){
             return;
         }
-        if(grid[i][j]==-1){
-            return;
-        }
+        
         grid[i][j]='$';
         dfs(grid,i+1,j);
         dfs(grid,i-1,j);
