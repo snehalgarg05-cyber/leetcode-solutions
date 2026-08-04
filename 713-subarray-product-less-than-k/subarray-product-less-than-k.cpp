52ae775c9caff23d15c3b8c4a4bc8548e300e944
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-08-04 14:27:41 +0530

    Time: 4 ms (26.91%) | Memory: 44.8 MB (53.69%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,68 +1,22 @@
-// class Solution {  //brute force accepted
-// public:
-//     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-//         int n = nums.size();
-//         int count=0;
-//         for(int i=0;i<n;i++){
-//             long long product=1;
-//             for(int j=i;j<n;j++){
-//                 product*=nums[j];
-//                 if(product<k){
-//                     count++;
-//                 }
-//                 else{
-//                     break;
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// };
-
 class Solution {
 public:
-        int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-
-        if(k <= 1) return 0;
-
-        int start = 0;
-        long long product = 1;
-        int count = 0;
-
-        for(int end = 0; end < nums.size(); end++) {
-
-            product *= nums[end];
-
-            while(product >= k) {
-                product /= nums[start];
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n = nums.size();
+        int start=0,end=0;
+        int count=0;
+        long long prod=1;
+        if(k<=1){
+            return 0;
+        }
+        while(end<n){
+            prod*=nums[end];
+            while(prod>=k){
+                prod/=nums[start];
                 start++;
             }
-
-            count += (end - start + 1);
+            count+=end-start+1;
+            end++;
         }
-
         return count;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
