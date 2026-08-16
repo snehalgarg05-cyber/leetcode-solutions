e6a5c8050c59c7ed9b85dcb3631a0f05429d12db
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-08-16 22:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.58%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.cpp
+++ 2491-smallest-even-multiple/smallest-even-multiple.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        for(int i=1;i<=INT_MAX;i++){
+        for(int i=1;i<=2*n;i++){
             if(i%2==0 && i%n==0){
                 return i;
             }
