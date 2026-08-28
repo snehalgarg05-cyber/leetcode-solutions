ce450d51ed24bfa8117b12acce423086bae52954
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-08-29 00:47:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.57%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,27 +1,85 @@
+// class Solution { //brute force approach :- order of n cube
+// public:
+//     string shortestBeautifulSubstring(string s, int k) {
+//         int n = s.size();
+        
+//         for(int len = k;len<=n;len++){
+//             string result="";
+//             for(int start=0;start<=n-len;start++){
+//                 int count_one=0;
+//                 string temp = s.substr(start,len);
+//                 for(char &ch : temp){
+//                     count_one+=(ch=='1')?1:0;
+//                 }
+//                 if(count_one==k){
+//                     if(result.empty() || temp<result){
+//                         result=temp;
+//                     }
+//                 }
+//             }
+//             if(!result.empty()){
+//                 return result;
+//             }
+//         }
+        
+//         return "";
+//     }
+// };
+
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.size();
-        
-        for(int len = k;len<=n;len++){
-            string result="";
-            for(int start=0;start<=n-len;start++){
-                int count_one=0;
-                string temp = s.substr(start,len);
-                for(char &ch : temp){
-                    count_one+=(ch=='1')?1:0;
+        int start=0,end=0;
+        string result = "";
+        int one_cnt=0;
+        while(end<n){
+            if(s[end]=='1'){
+                one_cnt++;
+            }
+            while(one_cnt>k || s[start]=='0'){
+                if(s[start]=='1'){
+                    one_cnt--;
                 }
-                if(count_one==k){
-                    if(result.empty() || temp<result){
-                        result=temp;
-                    }
+                start++;
+            }
+            if(one_cnt==k){
+                string temp = s.substr(start,end-start+1);
+                if(result.empty() || result.size()>end-start+1 || (result.size()==temp.size() && temp<result)){
+                    result = temp;
                 }
             }
-            if(!result.empty()){
-                return result;
-            }
+            end++;
         }
-        
-        return "";
+        return result;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
