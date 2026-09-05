788efc48f08d585209e506def44dd901018907c2
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-09-05 19:50:42 +0530

    Time: 3 ms (95.16%) | Memory: 34.2 MB (48.57%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,43 +1,93 @@
+// class Solution {
+// public:
+
+//     int m, n;
+
+//     int dfs(vector<vector<int>>& grid, int i, int j) {
+
+//         if (i < 0 || i >= m || j < 0 || j >= n)
+//             return 0;
+
+//         if (grid[i][j] != 1)
+//             return 0;
+
+//         // If this cell is boundary,
+//         // entire component is invalid
+//         if (i == 0 || i == m - 1 ||
+//             j == 0 || j == n - 1) {
+
+//             grid[i][j] = -1;
+
+//             dfs(grid, i + 1, j);
+//             dfs(grid, i - 1, j);
+//             dfs(grid, i, j + 1);
+//             dfs(grid, i, j - 1);
+
+//             return -1;   // component touches boundary
+//         }
+
+//         grid[i][j] = -1;
+
+//         int down  = dfs(grid, i + 1, j);
+//         int up    = dfs(grid, i - 1, j);
+//         int right = dfs(grid, i, j + 1);
+//         int left  = dfs(grid, i, j - 1);
+
+//         // If any part of component touches boundary
+//         if (down < 0 || up < 0 || right < 0 || left < 0)
+//             return -1;
+
+//         return 1 + down + up + right + left;
+//     }
+
+//     int numEnclaves(vector<vector<int>>& grid) {
+
+//         m = grid.size();
+//         n = grid[0].size();
+
+//         int count = 0;
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+
+//                 if (grid[i][j] == 1) {
+
+//                     int x = dfs(grid, i, j);
+
+//                     if (x > 0)
+//                         count += x;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
 
     int m, n;
 
-    int dfs(vector<vector<int>>& grid, int i, int j) {
+    void dfs(vector<vector<int>>& grid, int i, int j) {
 
+        // Out of bounds
         if (i < 0 || i >= m || j < 0 || j >= n)
-            return 0;
+            return;
 
+        // Not land
         if (grid[i][j] != 1)
-            return 0;
+            return;
 
-        // If this cell is boundary,
-        // entire component is invalid
-        if (i == 0 || i == m - 1 ||
-            j == 0 || j == n - 1) {
-
-            grid[i][j] = -1;
-
-            dfs(grid, i + 1, j);
-            dfs(grid, i - 1, j);
-            dfs(grid, i, j + 1);
-            dfs(grid, i, j - 1);
-
-            return -1;   // component touches boundary
-        }
-
+        // Mark visited
         grid[i][j] = -1;
 
-        int down  = dfs(grid, i + 1, j);
-        int up    = dfs(grid, i - 1, j);
-        int right = dfs(grid, i, j + 1);
-        int left  = dfs(grid, i, j - 1);
-
-        // If any part of component touches boundary
-        if (down < 0 || up < 0 || right < 0 || left < 0)
-            return -1;
-
-        return 1 + down + up + right + left;
+        // 4 directions
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 
     int numEnclaves(vector<vector<int>>& grid) {
@@ -45,18 +95,34 @@ public:
         m = grid.size();
         n = grid[0].size();
 
+        // DFS from left and right boundaries
+        for (int i = 0; i < m; i++) {
+
+            if (grid[i][0] == 1)
+                dfs(grid, i, 0);
+
+            if (grid[i][n - 1] == 1)
+                dfs(grid, i, n - 1);
+        }
+
+        // DFS from top and bottom boundaries
+        for (int j = 0; j < n; j++) {
+
+            if (grid[0][j] == 1)
+                dfs(grid, 0, j);
+
+            if (grid[m - 1][j] == 1)
+                dfs(grid, m - 1, j);
+        }
+
+        // Count remaining 1s
         int count = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
 
-                if (grid[i][j] == 1) {
-
-                    int x = dfs(grid, i, j);
-
-                    if (x > 0)
-                        count += x;
-                }
+                if (grid[i][j] == 1)
+                    count++;
             }
         }
 
