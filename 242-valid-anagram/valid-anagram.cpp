7d58203f182dcf0b45b6bbb05dbdf7b6949b2806
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-09-12 01:17:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (75.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
         vector<int> freq1(26,0);
         vector<int> freq2(26,0);
         int n = s.size();
+        int m = t.size();
         for(int i=0;i<n;i++){
             freq1[s[i]-'a']++;
+        }
+        for(int i=0;i<m;i++){
             freq2[t[i]-'a']++;
         }
         if(freq1==freq2){
