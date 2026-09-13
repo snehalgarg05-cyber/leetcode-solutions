ec7026a06c9df7065aa8ad7665406566c63b4266
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-09-13 12:55:43 +0530

    Time: 20 ms (10.00%) | Memory: 32.8 MB (90.00%) - LeetSync

--- 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
+++ 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
@@ -2,27 +2,15 @@ class Solution {
 public:
     int countSpecialIntegers(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> mp;
-        int count=0;
+        unordered_map<int,vector<int>>mp;
         for(int i=0;i<n;i++){
-            mp[nums[i]]++;
+            mp[nums[i]].push_back(i);
         }
-        for(int i=0;i<n;i++){
-            if(mp[nums[i]]==3){
-                int i1=i,i2=0,i3=0,first=0;
-                for(int j=i+1;j<n;j++){
-                    if(nums[j]==nums[i] && first==0){
-                        i2=j;
-                        first++;
-                        break;
-                    }
-                }
-                for(int j=i+1;j<n;j++){
-                    if(nums[j]==nums[i] && first==1){
-                        i3=j;
-                    }
-                }
-                if(i2-i1==i3-i2){
+        int count=0;
+        for(auto & it:mp){
+            if(it.second.size()==3){
+                vector<int> pos = it.second;
+                if(pos[1]-pos[0]==pos[2]-pos[1]){
                     count++;
                 }
             }
