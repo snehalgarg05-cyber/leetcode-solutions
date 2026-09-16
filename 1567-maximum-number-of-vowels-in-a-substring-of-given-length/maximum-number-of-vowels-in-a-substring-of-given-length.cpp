32e8bbd7fdbebaf4c29db4995e9b83b637734793
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-09-17 02:16:25 +0530

    Time: 8 ms (36.27%) | Memory: 13.3 MB (27.12%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,64 +1,29 @@
-// class Solution {
-// public:
-
-//     bool isVowel(char ch){
-//         return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
-//     }
-
-//     int maxVowels(string s, int k) {
-
-//         int n = s.size();
-
-//         // edge case
-//         if(n==0 || k==0) return 0;
-
-//         int maxCount = 0;
-
-//         // check every substring of length k
-//         for(int i=0; i<=n-k; i++){
-
-//             int count = 0;
-
-//             for(int j=i; j<i+k; j++){
-
-//                 if(isVowel(s[j])){
-//                     count++;
-//                 }
-//             }
-
-//             maxCount = max(maxCount, count);
-//         }
-
-//         return maxCount;
-//     }
-// };
-
 class Solution {
-    public:
+public:
 
-        bool isVowels(char &ch){
-            return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
+    bool isVowel(char ch){
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
+    }
 
-        }
-
-        int maxVowels(string s, int k)
-        {
-            int n=s.size();
-            int i=0, j=0, count=0, maxV=0;
-            while(j<n){
-                if(isVowels(s[j])){
-                    count++;
-                }
-                if(j-i+1==k){
-                    maxV= max(maxV, count);
-                    if(isVowels(s[i])){
-                        count--;
-                    }
-                    i++;
-                }
-                j++;
+    int maxVowels(string s, int k) {
+        int vow_cnt=0,cnt=0;
+        int n = s.size();
+        int start=0,end=0;
+        for(int i=0;i<k;i++){
+            if(isVowel(s[i])){
+                cnt++;
             }
-            return maxV;
-            
-        } 
+        }
+        vow_cnt = max(vow_cnt,cnt);
+        for(int i=k;i<n;i++){
+            if(isVowel(s[i-k])){
+                cnt--;
+            }
+            if(isVowel(s[i])){
+                cnt++;
+            }
+            vow_cnt = max(vow_cnt,cnt);
+        }
+        return vow_cnt;
+    }
 };
